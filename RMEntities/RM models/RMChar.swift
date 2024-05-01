9b0aa382efd2751ab36bde48598f73b304f584ec
Nikolai Kharkevich <>
2024-05-01 18:42:40 +0300

    feature/RM-1: Add RM Entities target

---
+++ RMEntities/RM models/RMChar.swift
@@ -0,0 +1,13 @@
+//
+//  RMChar.swift
+//  RMEntities
+//
+//  Created by Nikolai Kharkevich on 01.05.2024.
+//
+
+import Foundation
+
+public struct RMChar {
+    
+    public let name: String
+}
---
+++ RMEntities/RMEntities.h
@@ -0,0 +1,18 @@
+//
+//  RMEntities.h
+//  RMEntities
+//
+//  Created by Nikolai Kharkevich on 01.05.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RMEntities.
+FOUNDATION_EXPORT double RMEntitiesVersionNumber;
+
+//! Project version string for RMEntities.
+FOUNDATION_EXPORT const unsigned char RMEntitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RMEntities/PublicHeader.h>
+
+
