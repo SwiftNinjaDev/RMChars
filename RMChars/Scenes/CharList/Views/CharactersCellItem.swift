1186a132ed19f0e0aadd6536eb011e709fdc357c
Nikolai Kharkevich <>
2024-05-02 14:57:42 +0300

    feature/RM-1: Add helpers

---
+++ RMChars/Scenes/CharList/Views/CharactersCellItem.swift
@@ -0,0 +1,8 @@
+//
+//  CharactersCellItem.swift
+//  RMChars
+//
+//  Created by Nikolai Kharkevich on 02.05.2024.
+//
+
+import Foundation
---
+++ RMChars/Scenes/CharList/Views/CharactersTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  CharactersTableViewCell.swift
+//  RMChars
+//
+//  Created by Nikolai Kharkevich on 02.05.2024.
+//
+
+import Foundation
